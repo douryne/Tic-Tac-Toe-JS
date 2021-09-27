eaa836a462e12c1d5afa6ddbaa48bd917aff58d3
douryne <douryne.1st@gmail.com>
2021-09-27 13:16:10 +0500

    color cell on click refactored

--- script.js
+++ script.js
@@ -1,69 +1,10 @@
 let board = document.querySelector('table');
-let cellObj = document.querySelectorAll('td')
-
-let firstColor = "rgb(173, 216, 230)";
-let secondColor =  "rgb(255, 255, 119)";
 
 let crossTurn = true;
 
-board.addEventListener("mousedown", function(e) {
-    if(e.target.style.backgroundColor === '') {
-        e.target.style.backgroundColor = (crossTurn) ? firstColor : secondColor;
+board.addEventListener("mousedown", (e) => {
+    if(e.target.dataset.movedBy === undefined) {
+        e.target.dataset.movedBy = (crossTurn) ? "cross" : "zero";
         crossTurn = !crossTurn;
-        alertResultAndRestartGame();
     }
 })
-
-function alertResultAndRestartGame() {
-
-    if(isOver() == 0) alert("Cross won!");
-    else if(isOver() == 1) alert("Zero won!");
-    else if(isOver() == 2) alert("Draw!");
-    else return;
-
-    cellObj.forEach(cell => cell.style.backgroundColor = '');
-    crossTurn = true;
-
-}
-
-function isOver() {
-
-    for(let cell = 0; cell < 3; cell++) {
-        if(cellObj[cell].style.backgroundColor === cellObj[cell+3].style.backgroundColor
-            && cellObj[cell].style.backgroundColor === cellObj[cell+6].style.backgroundColor
-            && cellObj[cell].style.backgroundColor !== '') {
-                return (cellObj[cell].style.backgroundColor === firstColor) ? 0 : 1;
-            }
-    }
-    
-    for(let cell = 0; cell < 7; cell+=3) {
-        if(cellObj[cell].style.backgroundColor === cellObj[cell+1].style.backgroundColor
-            && cellObj[cell].style.backgroundColor === cellObj[cell+2].style.backgroundColor
-            && cellObj[cell].style.backgroundColor !== '') {
-                return (cellObj[cell].style.backgroundColor === firstColor) ? 0 : 1;
-            }
-    }
-
-    if(cellObj[0].style.backgroundColor === cellObj[4].style.backgroundColor
-        && cellObj[0].style.backgroundColor === cellObj[8].style.backgroundColor
-        && cellObj[0].style.backgroundColor !== '') {
-            return (cellObj[0].style.backgroundColor === firstColor) ? 0 : 1;
-        }
-    else if(cellObj[2].style.backgroundColor === cellObj[4].style.backgroundColor
-            && cellObj[2].style.backgroundColor === cellObj[6].style.backgroundColor
-            && cellObj[2].style.backgroundColor !== '') {
-                return (cellObj[2].style.backgroundColor === firstColor) ? 0 : 1;
-        }
-
-
-    let counter = 0;
-
-    cellObj.forEach(cell => {
-        if(cell.style.backgroundColor !== '') counter++;
-    })
-
-    if(counter === cellObj.length) return 2;
-
-
-}
-
