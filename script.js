8ad8141f059ddf60f2e95b73936f0f811675ca40
douryne <douryne.1st@gmail.com>
2021-09-28 17:40:38 +0500

    draw condition refactored

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
-let board = document.querySelector('table');
-
-let cellArr = Array.from(document.querySelectorAll('td'));
-
+const board = document.querySelector("table");
+const cells = Array.from(document.querySelectorAll("td"));
 let crossTurn = true;
 
-board.addEventListener("mousedown", (e) => {
-    if(!e.target.dataset.movedBy) {
-        e.target.dataset.movedBy = (crossTurn) ? "cross" : "zero";
-        crossTurn = !crossTurn;
-        alertResult();
-    }
+board.addEventListener("mousedown", ({ target }) => {
+    if(target.dataset.movedBy) return;
+
+    target.dataset.movedBy = crossTurn ? "cross" : "zero";
+    crossTurn = !crossTurn;
+
+    alertResult();
 })
 
 function alertResult() {
-    if(whoWon() == 0) alert("X winner!");
-    else if(whoWon() == 1) alert("0 winner!");
-    else if(whoWon() == 2) alert("Draw!");
-    else return;
+    const winner = getWinner();
+    if(!winner) return;
+    alert(winner);
+    resetGame();
+}
 
-    cellArr.forEach(cell => delete cell.dataset.movedBy);
+function resetGame() {    
+    cells.forEach(cell => delete cell.dataset.movedBy);
     crossTurn = true;
 }
 
-function whoWon() {
-    let counter = cellArr.reduce((sum, cell) => {
-        if(cell.dataset.movedBy !== undefined) sum += 1;
-        return sum;
-    }, 0)
+function getWinner() {
+    // if(noWinner) return undefined;
+    // if() return "X winner!";
+    // if() return "0 winner!";
+    const cellsColored = cells.filter((cell) => cell.dataset.movedBy);
 
-    if(counter === cellArr.length) return 2;
+    if(cellsColored.length === cells.length) return "Draw!";
 }
